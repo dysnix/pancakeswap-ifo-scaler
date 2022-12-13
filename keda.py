c5dd45a538adef337d104683ce5c0f218fa7c122
Daniel Yavorovych <daniel@dysnix.com>
2022-12-13 16:34:21 +0200

    fix update only on change

--- keda.py
+++ keda.py
@@ -15,6 +15,7 @@ from settings import K8S_REPLICAS_COUNT, HOURS_BEFORE_SCALE, TARGET_NAME, TARGET
 class Keda:
     KEDA_API = "keda.sh"
     KEDA_VERSION = "v1alpha1"
+    TRIGGERS_CONF_PATH = "./conf/triggers.yaml"
 
     def __init__(self):
         self.logger = logging.getLogger('keda')
@@ -39,25 +40,37 @@ class Keda:
         template = self.jinja.get_template("scaledobject.yaml.jinja2")
         return template.render(**kwargs)
 
-    def __get_hash_from_json(self, data):
+    def __get_hash_from_json(self, ifos, triggers):
         data_for_hash = []
-        for i in data:
+        for i in ifos:
             data_for_hash.append({'name': i['address'], 'start_block': i['start_block'], 'end_block': i['end_block']})
         text_data = json.dumps(data_for_hash, indent=4, sort_keys=True, default=str).encode("utf-8")
-        return hashlib.md5(text_data).hexdigest()
+        m = hashlib.sha256()
+        m.update(text_data)
+        m.update(yaml.dump_all(triggers).encode('utf-8'))
+        return m.hexdigest()
+
+    def get_triggers(self):
+        with open(self.TRIGGERS_CONF_PATH, 'r') as f:
+            data = yaml.safe_load(f)
+        return data
 
     def patch_scaledobject(self, ifos):
         ifo_triggers = []
         current_resource = None
-        ifos_hash = self.__get_hash_from_json(ifos)
+        triggers = self.get_triggers()
+        checksum = self.__get_hash_from_json(ifos, triggers)
 
         try:
             current_resource = self.customObjectApi.get_namespaced_custom_object(self.KEDA_API, self.KEDA_VERSION,
                                                                                  TARGET_NAMESPACE,
                                                                                  'scaledobjects', SCALEDOBJECT_NAME)
 
-            if ifos_hash == current_resource['metadata']['annotations'].get('ifo-scaler.predictkube.com/ifos-hash', ''):
-                logging.info('IFOs not changed. Exit.')
+            current_checksum = current_resource['metadata']['annotations'].get('ifo-scaler.predictkube.com/checksum', '')
+            print('current checksum:', current_checksum)
+            print('new checksum:', checksum)
+            if checksum == current_checksum:
+                logging.info('No changes needed. Exit.')
                 return []
         except ApiException:
             logging.warning('Scaledobject {}.{} not found. Creating...'.format(SCALEDOBJECT_NAME, TARGET_NAMESPACE))
@@ -73,12 +86,13 @@ class Keda:
                                                       target_api_version=TARGET_API_VERSION, target_kind=TARGET_KIND,
                                                       timezone=TIMEZONE,
                                                       replicas=K8S_REPLICAS_COUNT,
+                                                      triggers=yaml.safe_dump(triggers),
                                                       ifo_triggers=ifo_triggers,
-                                                      ifos_hash=ifos_hash)
+                                                      checksum=checksum)
 
         if DRY_RUN:
             self.logger.info('DRY_RUN mode: scaledobject_yml content below:')
-            print(scaledobject_yml)
+            logging.info(scaledobject_yml)
         else:
             if current_resource:
                 self.customObjectApi.patch_namespaced_custom_object(self.KEDA_API, self.KEDA_VERSION,
